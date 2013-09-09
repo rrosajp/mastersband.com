724ac4bfdadbe71b1848bf1df55610744143174a
Aidan Ryan <ryan.aidan@gmail.com>
2013-09-08 19:47:51 -0700

    homepage skeleton

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace mastersband.App_Start
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/CSS/styles")
+                .Include(
+                    "~/CSS/normalize.css",
+                    "~/CSS/site.css"
+                ));
+        }
+    }
+}
---
+++ Helpers/HelpHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mastersband.Helpers
+{
+    public static class HelpHelpers
+    {
+        public static MvcHtmlString Playlist(string sourceRelativePath)
+        {
+        }
+    }
+}
